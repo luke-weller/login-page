864a6e952be37fda7d38bfa3358c5cd8c12c2144
Luke Weller <33352024+myselfweller@users.noreply.github.com>
2021-11-22 17:39:16 +0000

    add login required field validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,36 @@
-import React, { useState } from "react";
 import "./App.css";
-import Axios from "axios";
 import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
 
 function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
  
   // return user data for personalisation when logged in
-  const getUser = () => {
-    Axios({
-      method: "GET",
-      withCredentials: true,
-      url: "http://localhost:4000/user",
-    }).then((res) => {
-      setData(res.data);
-      console.log(res.data);
-    });
-  };
+  // const getUser = () => {
+  //   Axios({
+  //     method: "GET",
+  //     withCredentials: true,
+  //     url: "http://localhost:4000/user",
+  //   }).then((res) => {
+  //     setData(res.data);
+  //     console.log(res.data);
+  //   });
+  // };
 
 
   return (
-    <div className="App">
-      <div>
+    <div className='Container'>
+      <div className='ChildContainer'>       
 
-      <Register />
-      <Login />
+            
+      <Login title="Login to authenticator" />
 
-      <div>
+      {/* <div>
         <h1>Get User</h1>
         <button onClick={getUser}>Submit</button>
         {data ? <h1>Welcome Back {data.username}</h1> : null}
+      </div> */}
       </div>
-    </div> </div>
+    </div>
   );
 }
 
