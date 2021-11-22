85e153c279ab5c6ac293182c14aa9c7f7cdde104
Luke Weller <33352024+myselfweller@users.noreply.github.com>
2021-11-22 15:51:31 +0000

    refactor functionality into components

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import Axios from "axios";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginUsername, setLoginUsername] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
   const [data, setData] = useState(null);
-  const register = () => {
-    Axios({
-      method: "POST",
-      data: {
-        username: registerUsername,
-        password: registerPassword,
-      },
-      withCredentials: true,
-      url: "http://localhost:4000/register",
-    }).then((res) => console.log(res));
-  };
-  const login = () => {
-    Axios({
-      method: "POST",
-      data: {
-        username: loginUsername,
-        password: loginPassword,
-      },
-      withCredentials: true,
-      url: "http://localhost:4000/login",
-    }).then((res) => console.log(res));
-  };
+ 
+  // return user data for personalisation when logged in
   const getUser = () => {
     Axios({
       method: "GET",
@@ -40,40 +18,21 @@ function App() {
       console.log(res.data);
     });
   };
+
+
   return (
     <div className="App">
       <div>
-        <h1>Register</h1>
-        <input
-          placeholder="username"
-          onChange={(e) => setRegisterUsername(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          onChange={(e) => setRegisterPassword(e.target.value)}
-        />
-        <button onClick={register}>Submit</button>
-      </div>
 
-      <div>
-        <h1>Login</h1>
-        <input
-          placeholder="username"
-          onChange={(e) => setLoginUsername(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          onChange={(e) => setLoginPassword(e.target.value)}
-        />
-        <button onClick={login}>Submit</button>
-      </div>
+      <Register />
+      <Login />
 
       <div>
         <h1>Get User</h1>
         <button onClick={getUser}>Submit</button>
         {data ? <h1>Welcome Back {data.username}</h1> : null}
       </div>
-    </div>
+    </div> </div>
   );
 }
 
