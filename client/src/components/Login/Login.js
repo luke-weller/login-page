22417519548e5f62b17d09c70a4619dc1e2a5063
Luke Weller <33352024+myselfweller@users.noreply.github.com>
2021-11-25 14:33:02 +0000

    add authentication to login component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,47 +1,61 @@
 import Axios from "axios";
-import { useState } from "react";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
+// validation scheema
+const schema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
+}).required();
 
 const Login = () => {
-    const [loginUsername, setLoginUsername] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
+  // validation
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const userAuthentication = () => {
+    // logic to authenticate a user
+    const userAuthentication = (data) => {
         Axios({
           method: "POST",
           data: {
-            username: loginUsername,
-            password: loginPassword,
+            username: data.username,
+            password: data.password,
           },
           withCredentials: true,
           url: "http://localhost:4000/login",
         }).then((res) => console.log(res));
       };
 
-
     return (        
         <div>
           <h1>Login</h1>
-          <form>
+          <form onSubmit={handleSubmit(userAuthentication)} noValidate>
+
             {/* username field */}
             <div>
               <label htmlFor='username'>Username</label>
               <input
-                type="text"
-                onChange={(e) => setLoginUsername(e.target.value)}
+                name='username'
+                type='text'
+                {...register('username')}
               />
+              <p className='ErrorText'>{errors.username?.message}</p>
             </div>
 
             {/* password field */}
             <div>
               <label htmlFor='password'>Password</label>
                 <input
-                  type="password"
-                  onChange={(e) => setLoginPassword(e.target.value)}
+                  name='password'
+                  type='password'
+                  {...register('password')}
                 />
+                <p className='ErrorText'>{errors.password?.message}</p>
             </div>
 
-              <button onClick={userAuthentication}>Submit</button>
+              <button>Submit</button>
           </form>
         </div>
     )
